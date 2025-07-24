c8feb811ceebfd500ed362e006698e39562009db
PinBib <105994863+Pinbib@users.noreply.github.com>
2025-07-24 22:19:32 +0100

    updated Rollup configuration
    added .d.ts assembly

--- rollup.config.js
+++ rollup.config.js
@@ -1,12 +1,27 @@
 import {nodeResolve} from '@rollup/plugin-node-resolve';
 import terser from "@rollup/plugin-terser";
+import {dts} from "rollup-plugin-dts";
 
-export default {
-	input: "dist/index.js",
-	output: {
-		file: "bundle/index.js",
-		plugins: [terser()]
+export default [
+	{
+		input: "dist/index.js",
+		output: [
+			{
+				file: "bundle/index.min.js",
+				plugins: [terser()]
+			}, {
+				file: "bundle/index.js"
+			}
+		],
+		external: [],
+		plugins: [nodeResolve()]
 	},
-	external: [],
-	plugins: [nodeResolve()]
-};
+	{
+		input: "dist/d/index.d.ts",
+		output: {
+			file: "bundle/d/index.d.ts"
+		},
+		external: [],
+		plugins: [nodeResolve(), dts()]
+	}
+];
